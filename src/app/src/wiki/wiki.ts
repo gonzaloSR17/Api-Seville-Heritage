c2bd53c5c73d0bef564b8f4a34137c2a2d6f63ba
gonzaloSR17 <gonzalosanchezros@gmail.com>
2026-05-12 18:16:55 +0200

    12/05/2026 - 18:16

--- src/app/src/wiki/wiki.ts
+++ src/app/src/wiki/wiki.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { WikiMain } from './wiki-main/wiki-main';
 
 @Component({
   selector: 'app-wiki',
-  imports: [],
+  imports: [WikiMain],
   templateUrl: './wiki.html',
   styleUrl: './wiki.css',
 })
--- src/main-end/main-end.ts
+++ src/main-end/main-end.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-main-end',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './main-end.html',
   styleUrl: './main-end.css',
 })
