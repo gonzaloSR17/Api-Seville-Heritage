491cbe385faa6a4594d712a62506ad2e485306b9
gonzaloSR17 <gonzalosanchezros@gmail.com>
2026-05-13 18:10:53 +0200

    13/05/2026 18:10 layout-wiki

--- src/app/src/wiki/wiki.ts
+++ src/app/src/wiki/wiki.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { WikiMain } from './wiki-main/wiki-main';
+import { WikiSidebar } from './wiki-sidebar/wiki-sidebar';
 
 @Component({
   selector: 'app-wiki',
-  imports: [WikiMain],
+  imports: [WikiMain, WikiSidebar],
   templateUrl: './wiki.html',
   styleUrl: './wiki.css',
 })
