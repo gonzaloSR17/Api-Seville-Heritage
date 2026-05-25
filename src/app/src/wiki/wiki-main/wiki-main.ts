bcd2c2df0946febe17a2720690e7b7990dde6792
gonzaloSR17 <gonzalosanchezros@gmail.com>
2026-05-25 18:41:55 +0200

    25/05/2026 - 18:41

--- src/app/src/wiki/wiki-main/wiki-main.ts
+++ src/app/src/wiki/wiki-main/wiki-main.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ChangeDetectorRef, inject } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-wiki-main',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './wiki-main.html',
   styleUrl: './wiki-main.css',
 })
@@ -13,26 +14,36 @@ export class WikiMain implements OnInit {
   loadingWindow: boolean = true;
   errorWindow: boolean = false;
 
+  totalPages = 0;
+  countPage = 1;
+  pages: number[] = [];
+
   // Dentro de tu clase:
   private cdr = inject(ChangeDetectorRef);
 
-  apiURL: string = 'https://backend-api-seville-heritage.onrender.com/data?_page=1&_limit=15'
-
-
   ngOnInit(): void {
-    this.obtenerCiudades();
+    this.obtenerCiudades(12);
   }
 
   // Funcion asincrona se ejecuta en segundo plano
-  async obtenerCiudades() {
+  async obtenerCiudades(numero: number) {
     try {
-           
+
+      this.countPage = numero;
+
       // Almacenamos la respuesta del fetch
-      const response = await fetch(this.apiURL);
+      const response = await fetch('https://backend-api-seville-heritage.onrender.com/data?_page=' + this.countPage + '&_limit=15');
 
       if (!response.ok) {
         throw new Error(`Error HTTP: ${response.status}`);
       }
+
+      // Contamos el total y lo dividimos en 15 paginas
+      const total = Number(response.headers.get('x-total-count'));
+      this.totalPages = Math.ceil(total / 15);
+
+      this.pages = Array.from({ length: this.totalPages }, (_, i) => i + 1);
+      console.log("Paginas totales = ", this.pages)
 
       // Convertimos la respuesta a JSON
       this.edificios = await response.json();
@@ -48,4 +59,11 @@ export class WikiMain implements OnInit {
     }
   }
 
+  marcarPagina(numero: number) {
+    if (numero == this.countPage) {
+      return true
+    } else {
+      return false
+    }
+  }
 }
