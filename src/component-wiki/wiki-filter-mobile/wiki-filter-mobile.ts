995c89506ae33fa9839840e2aadeea4e2a932607
gonzaloSR17 <gonzalosanchezros@gmail.com>
2026-06-29 18:23:59 +0200

    29/06/2026 18:23

--- src/component-wiki/wiki-filter-mobile/wiki-filter-mobile.ts
+++ src/component-wiki/wiki-filter-mobile/wiki-filter-mobile.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FilterService } from '../../services/filter.service';
+import { EdificioPoo } from '../../models/edificio.model';
 
 @Component({
   selector: 'app-wiki-filter-mobile',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './wiki-filter-mobile.html',
   styleUrl: './wiki-filter-mobile.css',
 })
 export class WikiFilterMobile {
+
+  // Importamos el servicio de Inyeccion para la comunicación de los modulos, asi puedo pasar variables entre si
+  constructor(private filterService: FilterService) {}
+
+  // El objeto que se vincula directamente al HTML
+  edificioFiltrado: EdificioPoo = {
+    q: '',
+    architect: '',
+    category: '',
+    district: ''
+  };
+
+  // Funcion para lanzar el filtro
+  lanzarFiltro() {
+    console.log(this.edificioFiltrado)
+    this.filterService.formarFiltro(this.edificioFiltrado)
+  }
+
+  // Limpiar filtro
+  limpiarFiltro() {
+    this.edificioFiltrado = {
+    q: '',
+    architect: '',
+    category: '',
+    district: ''
+  };
+
+  this.filterService.limpiarFiltros(this.edificioFiltrado);
+  }
 
 }
--- src/services/filter.service.ts
+++ src/services/filter.service.ts
@@ -5,11 +5,27 @@ import { EdificioPoo } from '../models/edificio.model';
 @Injectable({ providedIn: 'root' })
 export class FilterService {
 
+
+  formarFiltro(edificio: EdificioPoo) {
+      // Filtamos los valores vacios y los agregamos a una variable objetos
+      const filtrosActivos = Object.fromEntries(
+        // Object Entries asignamos a cada clave su valor y lo convertimos en un array de pares [clave, valor]
+        Object.entries(edificio).filter(([key, value]) => value && value !== 'Todos' && value !== 'Categoria') // Si valor existe y no es igual a todos
+      );
+
+      this?.setUrl(edificio);
+    }
+
   // Variable para almacenar la url del filtro  
   private urlSubject = new BehaviorSubject<EdificioPoo | null>(null);
   url$ = this.urlSubject.asObservable();
 
   setUrl(url: EdificioPoo) {
     this.urlSubject.next(url);
+  }
+
+  // Limpiar los filtros para que vuelva todo como estaba antes
+  limpiarFiltros(edificio: EdificioPoo){
+    this.setUrl(edificio);
   }
 }
